09822ba39b5e2219cb245d07d664024198cc366e
Rohith Tompe <rohithtompe5276@gmail.com>
2025-09-24 16:04:56 +0530

    rohith added

--- frontend/myproject/src/App.jsx
+++ frontend/myproject/src/App.jsx
@@ -1,30 +1,8 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
-// import TrainerDashboard from "./pages/TrainerDashboard/TrainerDashboard";
-// import TraineeDashboard from "./pages/TraineeDashboard/TraineeDashboard";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-//         <Route path="/trainer-dashboard" element={<TrainerDashboard />} />
-//         <Route path="/trainee-dashboard" element={<TraineeDashboard />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
-import TrainerDashboard from "./pages/TrainerDashboard/TrainerDashboard";
-import TraineeDashboard from "./pages/TraineeDashboard/TraineeDashboard";
+import AdminDashboard from "../src/components/AdminDashboard/AdminDashboard";
+import TrainerDashboard from "../src/components/TrainerDashboard/TrainerDashboard";
+import TraineeDashboard from "../src/components/TraineeDashboard/TraineeDashboard";
 
 function App() {
   return (
